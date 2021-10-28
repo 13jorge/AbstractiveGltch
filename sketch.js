3c397ca40e9da5e22b1243fefca111a802d4d6bf
Shaduwz <jorgejimenez004@gmail.com>
2021-10-28 01:51:39 -0700

    it shows on the web

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 let img;
 let cnv;
 function  preload(){
-  img = loadImage('assests/port.jpg');
+  img = loadImage('images/port.jpg');
 }
 function setup() {
   cnv = createCanvas(img.width, img.height);
-    image(img,0,0);
-  //print(img.width, img.height)
+    image(img,0,0);  
   //let newCanvasX = (windowWidth - img.width)/2;
   //let newCanvasY= (windowHeight - img.height)/2;
   //cnv.position(newCanvasX, newCanvasY);
@@ -77,7 +76,7 @@ function mouseMoved(){
     }
   } else{
    }
-    if (mouseY < img.height){
+    if (mouseY > img.height){
   
   let newCanvasX = (windowWidth - img.width);
   let newCanvasY= (windowHeight - img.height);
