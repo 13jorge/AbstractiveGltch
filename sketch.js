d0f9fba5e890e8e028a7b56a80ec751e409980d3
Shaduwz <jorgejimenez004@gmail.com>
2021-12-02 11:38:33 -0800

    Updated the website

--- sketch.js
+++ sketch.js
@@ -1,27 +1,34 @@
 let img;
 let para;
 let cnv;
+let abstract = [];
+let imageCounter = 0;
+let button;
+
 function  preload(){
-  img = loadImage('images/port.jpg');
+
+  for (let i =0; i <= 5; i++){
+    abstract[i] = loadImage("images/abstract" + i + ".png")
+  }
+  // img = loadImage('images/abstract0.png');
 }
 function setup() {
   para = createP('');
   para.parent("#instr"); 
-  cnv=createCanvas(img.width, img.height);
+  cnv=createCanvas(abstract[2].width, abstract[2].height);
   cnv.parent("#canvasImg");
-
-  image(img,0,0);  
+  image(random(abstract),0,0);
 }
 
 
 
 function mouseDragged(){
 
-  for(let col = 0;col< img.width; col+=random(50, 200)){
-    for(let row = 0; row<img.height; row+=random(50, 100)){
+  for(let col = 0;col<abstract[2].width; col+=random(50, 200)){
+    for(let row = 0; row<abstract[2].height; row+=random(50, 200)){
       let xPos = col;
       let yPos = row;
-      let c = img.get(xPos,yPos);
+      let c = random(abstract).get(xPos,yPos);
       
       
       push();
@@ -42,33 +49,75 @@ function mouseDragged(){
 }
 
 function keyPressed(){
-  for(let col = 0;col< img.width; col+=random(50, 100)){
-    for(let row = 0; row<img.height; row+=random(50, 100)){
+  for(let col = 0;col< abstract[2].width; col+=random(50, 100)){
+    for(let row = 0; row< abstract[2].height; row+=random(50, 100)){
       let xPos = col;
       let yPos = row;
-      let c = img.get(col,row);
+      let c = abstract[2].get(col,row);
 
   if (key === 'r'){
-       fill(color('red'));
-      rect(col,row, random(100),random(10))
-      } 
-      
-      //   stroke(color('blue'));
-      // } else if(key === 'g'){
-      //   stroke(color('green'));
-      // }   
+    push();
+    translate(xPos, yPos);
+    rotate(radians(random(360)))
+    noFill();
+    stroke(color('#FF6363 '));  
+    strokeWeight(random(10))
+    point(xPos,yPos);
+    strokeWeight(random(3));
+    //curve(x1, y1, x2, y2, x3, y3, x4, y4)
+    curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), random(10), random(80), cos(yPos) * sin(yPos) * random(140), cos(xPos) * sin(xPos) * 50)
 
-      // fill(color(c));
-      // rect(col,row, random(10),random(100))
+    pop();
       }
+      
+      if (key === 'b'){
+        push();
+        translate(xPos, yPos);
+        rotate(radians(random(360)))
+        noFill();
+    
+        stroke(color('#5FF4FA'));  
+        strokeWeight(random(10))
+        point(xPos,yPos);
+        strokeWeight(random(3));
+        //curve(x1, y1, x2, y2, x3, y3, x4, y4)
+        curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), random(10), random(80), cos(yPos) * sin(yPos) * random(140), cos(xPos) * sin(xPos) * 50)
+    
+        pop();
+          }
+
+            if (key === 'w'){
+              push();
+              translate(xPos, yPos);
+              rotate(radians(random(360)))
+              noFill();
+          
+              stroke(color('white'));  
+              strokeWeight(random(10))
+              point(xPos,yPos);
+              strokeWeight(random(3));
+              //curve(x1, y1, x2, y2, x3, y3, x4, y4)
+              curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), random(10), random(80), cos(yPos) * sin(yPos) * random(140), cos(xPos) * sin(xPos) * 50)
+          
+              pop();
+                }
+
+
+      }
+
+
+
     }
 
 }
 
+// #5FF4FA
+// lightblue
+
 function keyTyped(){
   if (key === 's'){
     saveCanvas('fileName', 'png');
-  }  if (key === 'i'){
-    image(img,0,0);
+  }  if (key === 'c'){
+    image(random(abstract),0,0);
   }
 }
